9f2020c44936f23fea1d605248c7ba163273df48
Jesse Reichman <jesse.reichman@noveris.com.au>
2023-11-20 13:09:14 +1030

    Convert to submodules. Add certmanager certs

--- resources/elastic/config/usersetup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-touch /usr/share/elasticsearch/config/users
-touch /usr/share/elasticsearch/config/users_roles
-
-/usr/share/elasticsearch/bin/elasticsearch-users useradd "$LOCALADMIN_USERNAME" -p "$LOCALADMIN_PASSWORD"
-/usr/share/elasticsearch/bin/elasticsearch-users passwd "$LOCALADMIN_USERNAME" -p "$LOCALADMIN_PASSWORD" || exit 1
-/usr/share/elasticsearch/bin/elasticsearch-users roles "$LOCALADMIN_USERNAME" -a superuser || exit 1
